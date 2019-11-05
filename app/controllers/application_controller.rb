253943c1e669ce6b841dc98f9b72583baee933f4
Harold Thiran <harold_1998@live.be>
2019-11-05 17:36:09 +0100

    fix little path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  def afer_sign_in_path_for(resource)
-  stored_location_for(resource) || foodtrucks_index_path
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || foodtrucks_path
   end
 end
