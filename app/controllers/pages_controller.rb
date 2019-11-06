cc624efb01c2dfd2c9063aa620fe112b94f27972
Arnaud Fiora <arnaud.fiora@gmail.com>
2019-11-06 14:09:24 +0100

    finished first version of dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,13 @@ class PagesController < ApplicationController
 
   def dashboard
     @foodtrucks = Foodtruck.where(user_id: current_user)
-    @bookings = Booking.where(user_id: current_user)
+    @bookings = Booking.all
+    @bookings_received = []
+
+    @bookings.each do |booking|
+      @bookings_received << booking if booking.foodtruck.user == current_user
+    end
+
+    @bookings_made = Booking.where(user: current_user)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_103300) do
+ActiveRecord::Schema.define(version: 2019_11_06_125939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
     t.string "date"
-    t.boolean "confirmed"
+    t.boolean "confirmed", default: false
     t.bigint "foodtruck_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
