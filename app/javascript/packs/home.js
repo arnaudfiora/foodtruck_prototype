42011f9ee1ea9e875cc5efc6b14f53aa939c0511
Arnaud Fiora <arnaud.fiora@gmail.com>
2019-11-08 13:37:28 +0100

    added js to toggle navbar transparency + changed banner image

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -3,3 +3,11 @@ const navbar = document.querySelector('.navbar-foodtruck')
 window.onload = (event) => {
   navbar.classList.add('transparent');
 };
+
+window.addEventListener('scroll', (event) => {
+  if (window.scrollY < 800) {
+    navbar.classList.add('transparent');
+  } else {
+    navbar.classList.remove('transparent');
+  }
+});
